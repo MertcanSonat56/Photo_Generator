c3e8b20a8a8a1963ecfbe24d7ccd06c457f47b52
MertcanSonat56 <mertcanyoldas7@gmail.com>
2025-09-13 02:54:56 +0300

    project init

---
+++ backend/src/test/java/com/wawex/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wawex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
