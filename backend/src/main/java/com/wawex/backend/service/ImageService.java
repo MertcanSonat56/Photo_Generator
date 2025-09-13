cf169a28b940f19ed67abcbe17573534f25eaaec
MertcanSonat56 <mertcanyoldas7@gmail.com>
2025-09-13 14:48:37 +0300

    frontend setup

---
+++ backend/src/main/java/com/wawex/backend/service/ImageService.java
@@ -0,0 +1,27 @@
+package com.wawex.backend.service;
+
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    private final OpenAiImageModel openAiImageModel;
+
+    public ImageService(OpenAiImageModel openAiImageModel) {
+        this.openAiImageModel = openAiImageModel;
+    };
+
+    public ImageResponse generateImage(String prompt, String quality, int n, int width, int height) {
+        
+        ImageResponse imageResponse = openAiImageModel.call(new ImagePrompt(prompt,
+                OpenAiImageOptions.builder()
+                        .quality("hd").N(4).height(1024).width(1024).build()));
+        
+        return imageResponse;
+    }
+
+}
